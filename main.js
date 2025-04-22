b8014b797059d5461ce7c2b0a442a08573da40e8
Leohuguenin <leonardohuguenin@live.com>
2025-04-22 11:30:35 +1000

    simple liker

--- main.js
+++ main.js
@@ -4,6 +4,26 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const likeButtons = document.getElementsByClassName("like-glyph");
+for (let element of likeButtons) {
+  element.addEventListener("click", function (event) {
+    mimicServerCall()
+      .then(() => {
+        const clickedElement = event.target;
+        clickedElement.classList.contains("activated-heart") ? 
+        clickedElement.classList.remove("activated-heart") : 
+        clickedElement.classList.add("activated-heart");
+      })
+      .catch(function () {
+        const errorMessage = document.getElementById("modal");
+        errorMessage.classList.remove("hidden");
+        setTimeout(() => {
+          errorMessage.classList.add("hidden")
+        }, 3000);
+      })
+  });
+}
+
 
 
 
@@ -11,9 +31,9 @@ const FULL_HEART = '♥'
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
         reject("Random server error. Try again.");
